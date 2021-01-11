109d851edf703d5decdfbb297b784b3ba77c2632
Michel Vollebregt <mi.vollebregt@gmail.com>
2021-01-11 16:55:25 +0100

    Initial Spring Boot project created by Spring Initializr

---
+++ src/main/java/com/github/mvollebregt/graphqldemoserver/GraphqlDemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mvollebregt.graphqldemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDemoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mvollebregt/graphqldemoserver/GraphqlDemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mvollebregt.graphqldemoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
